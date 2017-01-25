a0e66672151a4e36271d8376e05e5e21d1a52ed1
Ransom Byers <ransom.byers@gmail.com>
2017-01-24 23:47:32 -0800

    Add candidate evaluation method

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,6 @@
+
+require 'date'
+
 def questionnaire
 	file = {}
 
@@ -10,19 +13,45 @@ def questionnaire
 	puts "What year were you born?"
 	file['birth_year'] = gets.chomp
 
-	puts "Our company cafeteria serves garlic bread. Should we order 
-				some for you?"
-	file['order_bread'] = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	file['order_bread'] = gets.chomp.downcase.include?("yes")
 
 	puts "Would you like to enroll in the company's health insurance?"
-	file['enroll'] = gets.chomp
+	file['enroll'] = gets.chomp.downcase.include?("yes")
 
 	return file
 end
 
-def evaluate_candidate(file)
-	
+
+def age_right?(age,birth_year)
+	current_year = Date.today.year
+
+	return birth_year + age == current_year || 
+					birth_year + age == current_year - 1
 end
 
+
+def evaluate_candidate(file)
+	knows_age = age_right?(file['age'],file['birth_year'])
+	
+	result = "Results inconclusive"
+	if file['name'] == "Drake Cula" || file['name'] == "Tu Fang"
+		result = "Definitely a vampire"
+	elsif knows_age
+		if file['order_bread'] || filel['enroll']
+			result = "Probably not a vampire"
+		end
+	elsif !file['order_bread'] && !file['enroll']
+		result = "Almost certainly a vampire"
+	end
+
+	return result
+end		
+	
+
+
 f = questionnaire
-puts f['name'] + "!!!"
+
+puts age_right?(f['age'],f['birth_year'])
+
+puts evaluate_candidate(f)

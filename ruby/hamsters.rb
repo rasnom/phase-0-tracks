5112af460a9bd6ac61a76407c2b454a899a93203
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-01-26 17:49:55 -0500

    updated variable types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,22 +1,30 @@
 def questions 
 	puts "What is the hamster's name?"
 	name = gets.chomp
+	
 
 	puts "What is the volume level?"
-	level = gets.chomp
+	level = gets.chomp.to_i
+	
 
 	puts "What is the fur color?"
 	color = gets.chomp
+	
 
 	puts "Is it a good candidate for adoption?"
 	adoption = gets.chomp
+	
 
 	puts "What is it's age?"
 	age = gets.chomp
 
 	if age == ""
 		age = nil
+	else
+		age = age.to_i
+
 	end
+	
 end
 
 questions

a4dad9e5ad9df1927924f8c6ba0b81e1ef937377
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-01-26 17:57:45 -0500

    final draft

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,33 +1,31 @@
-def questions 
-	puts "What is the hamster's name?"
-	name = gets.chomp
-	
 
-	puts "What is the volume level?"
-	level = gets.chomp.to_i
-	
+puts "What is the hamster's name?"
+name = gets.chomp
 
-	puts "What is the fur color?"
-	color = gets.chomp
-	
+puts "What is the volume level?"
+level = gets.chomp.to_i
 
-	puts "Is it a good candidate for adoption?"
-	adoption = gets.chomp
-	
+puts "What is the fur color?"
+color = gets.chomp
 
-	puts "What is it's age?"
-	age = gets.chomp
+puts "Is it a good candidate for adoption?"
+adoption = gets.chomp
 
-	if age == ""
-		age = nil
-	else
-		age = age.to_i
-
-	end
-	
+puts "What is it's age?"
+age = gets.chomp
+if age == ""
+	age = nil
+else
+	age = age.to_i
 end
 
-questions
+if age != nil 
+	puts "#{name} is a #{color} hamster of #{age} years."
+	puts "it is #{level} loud and in the owners opinion, #{adoption} for adoption"
+else
+	puts "you have an ageless hamster!"
+end
+
 
 
 

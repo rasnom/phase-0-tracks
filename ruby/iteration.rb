74654552b89a109dd5f96625312000405334c3cf
Ransom Byers <ransom.byers@gmail.com>
2017-02-01 13:13:10 -0800

    Apply .map and .each

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,6 +4,22 @@ def status_update
 	puts "all done"
 end
 
-status_update { |name| puts "a string #{name}"} 
+# status_update { |name| puts "a string #{name}"} 
 
 
+colors = ['red','blue','green','periwinkle']
+
+books = {fiction: ['The Martian','Age of Innocense'],
+					nonfiction: ['Encyclopedia','Book on Ruby']}
+
+colors.each { |the_color| puts "this table color is #{the_color}"}
+
+books.each { |style, titles| 
+	puts "my favorite #{style} book is #{titles[0]}"}
+
+colors.map! do |the_color|
+	"dark " + the_color 
+end
+puts colors
+
+puts books.map {|style, titles| titles.reverse}

63383b01a4bae53694e28e118a69b8483bd92577
Ransom Byers <ransom.byers@gmail.com>
2017-02-02 01:32:37 -0800

    Still working on pulling info from nested hashes while not knowing the key for an intermediary layer, ie if you where looking for a park in a directory organized by city and knew the name of the park but not the city.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,9 +1,11 @@
 restaurant = {
-	menu: {
-		starters: {},
-		entrees: {},
-		desserts: {},
+ 	name: "Xenios",
+	menu_sections: {
+		starters: {section_title: "Starters"},
+		entrees: {section_title: "Entrees"},
+		desserts: {section_title: "Desserts"},
 		cocktails: {
+			section_title: "Cocktails",
 			nemesis: {
 				name: "Nemesis",
 				ingredients: ["Midori", "Agwa", "Housemade Sorrel Liqueur"],
@@ -24,10 +26,34 @@ restaurant = {
 	location: {
 		address: 	"1777 9th St",
 		city: "Oakland",
-		state: "CA",
+		state: "California",
 		phone: "510-555-555",
 		permit_number: "2017946070087"
 	}
 }
 
-puts restaurant
+puts restaurant 
+puts "\n\n"
+
+#############################################
+
+puts "Street Address:"
+puts restaurant[:name]
+street_address_fields = [:address, :city, :state]
+street_address_fields.each do |field_name| 
+	puts restaurant[:location][field_name]
+end
+puts "\n\n"
+
+##############################################
+
+# def Hash.find_nested_sibling(key,sibling_key)
+# 	self.each { |key, value| puts key}
+# end
+
+def list_ingredients(restaurant, dish)
+	puts dish
+	puts restaurant[:menu_sections]
+end
+
+list_ingredients(restaurant,:nemesis)

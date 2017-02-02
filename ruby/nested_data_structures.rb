55076c02d62422f4707b3de6edf418412401b93d
Ransom Byers <ransom.byers@gmail.com>
2017-02-01 21:17:36 -0800

    Start creating nested data structure for a restaurant

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+restaurant = {
+	menu: {
+		starters: [],
+		entrees: [],
+		desserts: [],
+		cocktails: []
+	}
+	location: = {
+		address: 	"1777 9th St"l,
+		city: "Oakland"
+		state: "CA"
+		phone: "510-555-555"
+		permit_number: "2017946070087"
+	}
+}

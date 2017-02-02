31fa632fb0dd113a42116f9c069b500ee0927c8c
Ransom Byers <ransom.byers@gmail.com>
2017-02-02 00:34:41 -0800

    Add a bunch of restaurant data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,15 +1,33 @@
 restaurant = {
 	menu: {
-		starters: [],
-		entrees: [],
-		desserts: [],
-		cocktails: []
-	}
-	location: = {
-		address: 	"1777 9th St"l,
-		city: "Oakland"
-		state: "CA"
-		phone: "510-555-555"
+		starters: {},
+		entrees: {},
+		desserts: {},
+		cocktails: {
+			nemesis: {
+				name: "Nemesis",
+				ingredients: ["Midori", "Agwa", "Housemade Sorrel Liqueur"],
+				price: 12
+			},
+			mystery_cult: {
+				name: "Mystery Cult",
+				ingredients: ["Blueberry Juice", "Gin", "Yuzu"],
+				price: 12
+			},
+			gladiator: {
+				name: "Gladiator's Cup",
+				ingredients: ["Mezcal", "Grapefruit Juice", "Lava Salt"],
+				price: 12
+			}
+		}
+	},
+	location: {
+		address: 	"1777 9th St",
+		city: "Oakland",
+		state: "CA",
+		phone: "510-555-555",
 		permit_number: "2017946070087"
 	}
 }
+
+puts restaurant

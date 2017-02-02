3ca507e7735207d9d31399f941b4d457a9e4c9d6
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-02 14:12:51 -0500

    Release 2: Bubble Sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,9 @@
 
+#Release 0
+
+arr = [2,33,4,5,9,2,112,23,43,4,45,6]
+
+
 def search_array(array, integer)
   array.each_with_index do |item, index|
     if item == integer
@@ -11,16 +16,14 @@ def search_array(array, integer)
 end
 
 
-
-arr = [2,33,4,5,9,2,112,23,43,4,45,6]
-
+p search_array(array,3)
 
 
 
 
 
 
-
+#Release 1
 
 def fib(integer)
   
@@ -42,12 +45,80 @@ def fib(integer)
   end
 
   return fib_list
-  
+
 end
 
 
 
  p fib(100)
+
+
+
+
+
+
+
+
+
+
+
+#Release 2
+
+#def bubble_sort(array)
+  #while the entire array is not sorted 
+    #Compare first element with second
+      #Swap if out of order
+    #Move to next comparison
+      #Swap
+
+
+array = [2,3,1,3,4,6,5,3,8,9,64,3,5,67,3]
+
+def bubble_sort(array)
+  
+  i_end = array.length-1 
+  while i_end > 0 
+    i = 0
+
+    while i < i_end
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+      end
+      i += 1
+    end  
+
+    i_end -= 1
+  end
+
+return array
+
+end
+
+
+p bubble_sort(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

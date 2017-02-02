67de2b2de05d241fb20ca82bf688d9ab6fc87f4f
Ransom Byers <ransom.byers@gmail.com>
2017-02-02 14:43:16 -0800

    Repeat the names and aliases back to the command line

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -43,8 +43,10 @@ name = ""
 until name == 'quit'
 	puts "Enter a name (or 'quit')"
 	name = gets.chomp
-	puts "becomes: #{fake_name(name)} \n\n"
 	monikers[name] = fake_name(name)
 end
 
-puts monikers
+puts "\n"
+monikers.each do |true_name, moniker|
+	puts "#{true_name} was once know as #{moniker}."	
+end

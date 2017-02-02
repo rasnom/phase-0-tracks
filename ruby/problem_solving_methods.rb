899f72ad813328139523fb53440839b44cb969ae
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-02 13:43:29 -0500

    Release 1: Fibonacci Numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,12 +12,43 @@ end
 
 
 
-
 arr = [2,33,4,5,9,2,112,23,43,4,45,6]
 
 
 
-puts search_array(arr,4)
+
+
+
+
+
+
+def fib(integer)
+  
+
+  if integer == 1
+    fib_list = [0]
+
+  elsif integer == 2
+    fib_list = [0,1]
+
+  else
+    fib_list = [0, 1]
+    counter = 3
+
+    while counter <= integer
+      fib_list << fib_list[counter-3] + fib_list[counter-2]
+      counter += 1
+    end
+  end
+
+  return fib_list
+  
+end
+
+
+
+ p fib(100)
+
 
 
 

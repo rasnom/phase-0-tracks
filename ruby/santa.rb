7e5efa5b792e2d073e65da184b4f99bfba3435e5
Ransom Byers <ransom.byers@gmail.com>
2017-02-04 00:40:27 -0800

    Finish attribute-changing methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -30,7 +30,20 @@ class Santa
 		@reindeer_ranking << reindeer
 	end
 
-	
+	def gender=(label)
+		@gender = label
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+
+
 
 end
 
@@ -38,10 +51,19 @@ end
 # s.speak
 # s.eat_milk_and_cookies("brownie")
 
-santas = []
-some_genders = ["forward", "backward", "innie", "outie", "pleaser", "cat"]
-some_ethnicities = ["new englander", "beige", "mutt", "roma", "aristocracy"]
 
-6.times {santas << Santa.new(some_genders.sample, some_ethnicities.sample)}
+# santas = []
+# some_genders = ["forward", "backward", "innie", "outie", "pleaser", "cat"]
+# some_ethnicities = ["new englander", "beige", "mutt", "roma", "aristocracy"]
+# 6.times {santas << Santa.new(some_genders.sample, some_ethnicities.sample)}
+# santas.each {|santa| santa.about}
 
-santas.each {|santa| santa.about}
+
+leo = Santa.new("hearty","Newfoundlander")
+p leo
+9.times {leo.celebrate_birthday}
+puts "age : #{leo.age}"
+puts "ethnicity : #{leo.ethnicity}"
+leo.gender = "thruster"
+leo.get_mad_at("Comet")
+p leo

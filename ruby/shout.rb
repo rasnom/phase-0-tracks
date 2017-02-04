4d0ac048aa0a0540ae28422523877678b418402a
Ransom Byers <ransom.byers@gmail.com>
2017-02-04 12:14:04 -0800

    Change shout module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,42 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		"w00t! " + words
+# 	end
+# end
+
+# puts Shout.yell_angrily("what is this nonsense?")
+
+# puts Shout.yell_happily("oh, ok.")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		"w00t! " + words
 	end
 end
 
-puts Shout.yell_angrily("what is this nonsense?")
+class Kitten
+	include Shout
 
-puts Shout.yell_happily("oh, ok.")
+	def initialize
+		puts yell_angrily("pay attention to me")
+	end
+end
+
+class Coworker
+	include Shout
+end	
+
+fuzz_demon = Kitten.new
+puts fuzz_demon.yell_happily("I love wet food")
+
+clyde = Coworker.new
+puts clyde.yell_angrily("Are both elevators broken again?")
+

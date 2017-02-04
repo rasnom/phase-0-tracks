3f13d3e8e849daaec4e3dbc7fec0fc34cda58fb3
Ransom Byers <ransom.byers@gmail.com>
2017-02-03 16:07:53 -0800

    Add puppy characteristics and behaviors note

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# CRITTER TYPE -------------------------------------
+# puppy
+
+# CHARACTERISTICS ----------------------------------
+# Weight: varies
+# Age: varies < 1 year
+# Legs: 4
+# Color: varies
+# Name: varies
+# Friendly: Defaults to true
+# Favorite Pooping Location: varies
+
+# BEHAVIOR ------------------------------------------
+# Snooze
+# Romp
+# Chew
+
+

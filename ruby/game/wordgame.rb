b08c5c8327dcc920fce97c510c405bd422acfcfd
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 13:16:57 -0800

    Add game_state variable and initialize method

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -1,3 +1,9 @@
 class WordGame
+	attr_reader :game_state
+
+	def initialize
+		@game_state = :startup
+	end
+
 
 end
--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -1,2 +1,9 @@
 require_relative 'wordgame'
 
+describe WordGame do
+	let(:wordgame) {WordGame.new}
+
+	it "new wordgames have a game_state of :startup" do
+		expect(wordgame.game_state).to eq :startup 
+	end
+end

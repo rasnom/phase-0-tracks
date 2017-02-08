c18b09fc9d22eaa1d3b0c2074e4f07f50159a146
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 13:40:54 -0800

    Add ability to start new rounds of the game

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -1,9 +1,17 @@
 class WordGame
-	attr_reader :game_state
+	attr_reader :game_state, :guesses_left, :progress
 
 	def initialize
 		@game_state = :startup
 	end
+
+	def new_round (secret_word)
+		@guesses_left = secret_word.length + 4
+		@progress = String.new
+		secret_word.length.times {progress << "_"}		
+		@game_state = :playing
+	end
+
 
 
 end
--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -3,7 +3,21 @@ require_relative 'wordgame'
 describe WordGame do
 	let(:wordgame) {WordGame.new}
 
-	it "new wordgames have a game_state of :startup" do
+	it "new wordgame objects have a game_state of :startup" do
 		expect(wordgame.game_state).to eq :startup 
+	end
+
+	it "starts a new game round" do
+		expect(wordgame.new_round("unimportant")).to eq :playing
+	end
+
+	it "new rounds set guesses left to secret word length plus 4" do
+		wordgame.new_round("party")
+		expect(wordgame.guesses_left).to eq 9
+	end
+
+	it "new rounds set progress to string of underscores" do
+		wordgame.new_round("something")
+		expect(wordgame.progress).to eq "_________"
 	end
 end

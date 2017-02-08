4c50fd04e57f3ab9f725618465b12c7c8cc6d3ec
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 10:14:01 -0800

    Add instance method tricks

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,36 @@ class Puppy
     toy
   end
 
+  def speak(count)
+  	count.times {puts "Woof!"}
+  end
+
+  def dog_years(human_years)
+  	human_years / 7
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def sit_stand(how_long_to_sit)
+  	how_long_to_sit.times { puts "sitting..."}
+  	puts "standing"
+  end
+
 end
 
+#####################################
+# Driver Code
+
+terrier = Puppy.new
+
+terrier.fetch("ball")
+
+terrier.speak(4)
+
+puts "20 human years is #{terrier.dog_years(20)} dog years"
+
+terrier.roll_over
+
+terrier.sit_stand(3)

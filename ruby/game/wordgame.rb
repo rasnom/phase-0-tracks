ad573623f6902954da5b41767d0206228cd9d3e5
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 15:30:51 -0800

    Add basic mvp driver code. Improve this later if nothing better to do.

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -58,3 +58,24 @@ class WordGame
 	end
 
 end
+
+
+#############################################
+# Driver Code
+
+game = WordGame.new
+
+puts "Shall we play a game?"
+sleep(1.5)
+puts "Of course we shall. Enter a secret word"
+
+game.new_round(gets.chomp)
+
+puts "Now guess letters in the word #{game.progress}."
+
+while game.keep_playing?
+	guess = gets.chomp
+	game.guess?(guess)
+	puts game.progress
+	puts game.message
+end

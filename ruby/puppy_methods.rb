d22016379e5b2b60a167b820096dce3bf8abced0
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 10:31:48 -0800

    Add Kitty class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -22,19 +22,56 @@ class Puppy
   	puts "standing"
   end
 
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+end
+
+class Kitty
+
+	def initialize
+		puts "Initializing kitty instance..."
+	end
+
+	def purr
+		puts "*purring*"
+	end
+
+	def rumpus(int)
+		int.times {puts "Kitten running about like a loon"}
+		return "chaos"
+	end
+
+	def push_cup(int)
+		if int == 1
+			puts "Kitten pushed #{int} cup off the table."
+		else
+			puts "Kitten pushed #{int} cups off the table."
+		end
+	end
+
 end
 
 #####################################
-# Driver Code
+# Puppy Driver Code
 
-terrier = Puppy.new
+# terrier = Puppy.new
+# terrier.fetch("ball")
+# terrier.speak(4)
+# puts "20 human years is #{terrier.dog_years(20)} dog years"
+# terrier.roll_over
+# terrier.sit_stand(3)
 
-terrier.fetch("ball")
+# Kitten Driver Code
 
-terrier.speak(4)
+kitties = []
 
-puts "20 human years is #{terrier.dog_years(20)} dog years"
+50.times do
+	kitties << Kitty.new
+end
 
-terrier.roll_over
-
-terrier.sit_stand(3)
+kitties.each do |cat|
+	cat.rumpus(3)
+	cat.push_cup(1)
+	cat.purr
+end

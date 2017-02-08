1c771084531e0ee5806f836e4fb7bdd392b2f737
Ransom Byers <ransom.byers@gmail.com>
2017-02-08 14:29:12 -0800

    Add progress tracker

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -6,12 +6,31 @@ class WordGame
 	end
 
 	def new_round (secret_word)
+		@secret_word = secret_word.upcase
 		@guesses_left = secret_word.length + 4
 		@progress = String.new
 		secret_word.length.times {progress << "_"}		
+		@letters_guessed = []
 		@game_state = :playing
 	end
 
+	def guess?(letter)
+		letter = letter.upcase
+		unless @letters_guessed.include?(letter) 
+			@guesses_left -= 1
+			@letters_guessed << letter
+		end
+		update_progress(letter)
+		@secret_word.include?(letter)
+	end
 
+	def update_progress(letter)
+		(0..@secret_word.length).each do |index|
+			if @secret_word[index] == letter
+				@progress[index] = letter
+			end
+		end	
+		@progress
+	end
 
 end
--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -20,4 +20,34 @@ describe WordGame do
 		wordgame.new_round("something")
 		expect(wordgame.progress).to eq "_________"
 	end
+
+	it "guess? returns true if it is in the word" do
+		wordgame.new_round("fetch")
+		expect(wordgame.guess?("t")).to eq true
+	end
+
+	it "ignores case in guesses" do
+		wordgame.new_round("TEST")
+		expect(wordgame.guess?("t")).to eq true
+	end
+
+	it "guessing a new letter decreases the guesses left" do
+		wordgame.new_round("party")
+		wordgame.guess?("a")
+		wordgame.guess?("b")
+		wordgame.guess?("c")
+		wordgame.guess?("c")
+		wordgame.guess?("C")
+		wordgame.guess?("C")
+		expect(wordgame.guesses_left).to eq 6
+	end
+
+	it "progress is updated with correctly guessed letters" do
+		wordgame.new_round("Populate")
+		wordgame.guess?("p")
+		wordgame.guess?("a")
+		wordgame.guess?("z")
+		expect(wordgame.progress).to eq "P_P__A__"
+	end
+	
 end

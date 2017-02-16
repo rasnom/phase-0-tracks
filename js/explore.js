9897504c459b59a56a4e8cae2acb849e1a54e869
Ransom Byers <ransom.byers@gmail.com>
2017-02-16 14:34:31 -0800

    Add skeleton of reverse function

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,11 @@
 // 	iterate backwards through string adding each letter to revstring
 // output: revstring
 
+function reverse(originalString) {
+	var reversedString = "";
+
+	return reversedString;
+}
+
+console.log("something")
+console.log(reverse("something"))

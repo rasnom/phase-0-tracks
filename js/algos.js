c07238c0d24b0720be033fb3e56b05992667ce93
Ransom Byers <ransom.byers@gmail.com>
2017-02-17 18:04:04 -0800

    Pseudocode longestPhrase function

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+function longest word/phrase
+input: array of words or phrases
+steps: 
+	make an empty string to store the longest phrase so far
+	loop through the array of phrases
+		if any are longer than the longest so far, replace it
+output: the longest phrase

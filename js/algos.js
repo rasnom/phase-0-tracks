d77dcef9c48304551bdc302088a02d9bc3a13fd4
Ransom Byers <ransom.byers@gmail.com>
2017-02-17 23:00:19 -0800

    Add longestPhrase driver code and fix mispelled variable

--- js/algos.js
+++ js/algos.js
@@ -8,12 +8,20 @@
 
 function longestPhrase(phraseList) {
 	var longestSoFar = ""
-	
+
 	for (var i = 0; i < phraseList.length; i++) {
 		if (longestSoFar.length < phraseList[i].length) {
-			lengestSoFar = phraseList[i]
+			longestSoFar = phraseList[i]
 		};
 	};
 
 	return longestSoFar;
+	
 };
+
+var activities = ['skiing','basking in the sun','snoozing in a hammock'];
+console.log(longestPhrase(activities));
+
+var codeKeys = ['34DK5L3','9O9ON','00203 FF','2 4 R','66766E-3'];
+console.log(longestPhrase(codeKeys));
+

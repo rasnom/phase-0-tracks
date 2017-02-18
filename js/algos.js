8db05d0b9160beddebed051e8b568e2ff0373169
Ransom Byers <ransom.byers@gmail.com>
2017-02-17 20:31:39 -0800

    Implement longestPhrase

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,19 @@
-function longest word/phrase
-input: array of words or phrases
-steps: 
-	make an empty string to store the longest phrase so far
-	loop through the array of phrases
-		if any are longer than the longest so far, replace it
-output: the longest phrase
+// function longest word/phrase
+// input: array of words or phrases
+// steps: 
+// 	make an empty string to store the longest phrase so far
+// 	loop through the array of phrases
+// 		if any are longer than the longest so far, replace it
+// output: the longest phrase
+
+function longestPhrase(phraseList) {
+	var longestSoFar = ""
+	
+	for (var i = 0; i < phraseList.length; i++) {
+		if (longestSoFar.length < phraseList[i].length) {
+			lengestSoFar = phraseList[i]
+		};
+	};
+
+	return longestSoFar;
+};

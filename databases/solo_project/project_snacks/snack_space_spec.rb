a47ac066401d24c00e88622550bd03666b5dcd0c
Ransom Byers <ransom.byers@gmail.com>
2017-02-24 21:07:17 -0800

    Add test for pulling down some manner of data by upc

--- databases/solo_project/project_snacks/snack_space_spec.rb
+++ databases/solo_project/project_snacks/snack_space_spec.rb
@@ -1,2 +1,12 @@
 require_relative 'snack_space'
 
+
+
+describe SnackInfo do
+	let (:snack_info) {SnackInfo.new}
+
+	it "can pull data from the gov by upc" do
+		expect(snack_info.upc_search(028400072731)).to be
+	end
+
+end

67a4bdd26b749bc6cf3d2467c8d17016eeda0475
Ransom Byers <ransom.byers@gmail.com>
2017-03-01 13:30:12 -0800

    Add background-color-contoller class that would eventually change all background colors on mouseover, but right now just sends an alert

--- web_dev/jquery/experimentation.js
+++ web_dev/jquery/experimentation.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
 	$("li").click(function(){
 		$(this).fadeOut();
 	});
@@ -8,5 +7,9 @@ $(document).ready(function(){
   $("li").dblclick(function(){
   	$("li").fadeIn();
   })
+
+  $(".background-color-controller").mouseenter(function(){
+  	alert("something")
+  });
 
 });
